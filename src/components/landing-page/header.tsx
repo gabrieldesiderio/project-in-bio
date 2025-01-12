50b82d3d204872146d92c2f01c07576f6fb81fc2
Gabriel Desidério <gabrieldesd7@gmail.com>
2025-01-11 22:09:27 -0300

    setup google account

--- src/components/landing-page/header.tsx
+++ src/components/landing-page/header.tsx
@@ -1,15 +1,23 @@
 import Image from "next/image";
 import { Button } from "../ui/button";
+import { auth } from "@/lib/auth";
+import { manageAuth } from "@/app/actions/manage-auth";
 
-export function Header() {
+export async function Header() {
+  const session = await auth()
+
+  console.log(session)
+
   return <div className="absolute top-0 left-0 right-0 max-w-7xl mx-auto flex items-center justify-between py-10">
     <div className="flex items-center gap-4">
       <Image src="/logo.svg" alt="ProjectInBio Logo" width={27} height={32} />
       <h3 className="text-white text-2xl font-bold">ProjectInBio</h3>
     </div>
     <div className="flex item-center gap-4">
-      <Button>Minha Página</Button>
-      <Button>Sair</Button>
+      {session && <Button>Minha Página</Button>}
+      <form action={manageAuth}>
+        <Button>{session ? 'Sair' : 'Login'}</Button>
+      </form>
     </div>
   </div>
 }
